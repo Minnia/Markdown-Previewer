a04f5189fe97ed7538b5ff96d987cd32d2d4fd65
Minnia <jasminestockholm@gmail.com>
2020-01-09 00:06:50 +0100

    first commit

---
+++ markdown-previewer/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import MarkdownPreview from "./components/MarkdownPreview";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MarkdownPreview />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ markdown-previewer/src/components/MarkdownPreview/MarkdownPreview.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+let ReactMarkdown = require("react-markdown");
+// * And last but not least, let's not forget embedded images:
+//  ![React Logo w/ Text](https://goo.gl/Umyytc )
+const MarkdownPreview = () => {
+  const [
+    markdownPreview,
+    setMarkdownPreview
+  ] = useState(`# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+
+  \`\`\`
+  // this is multi-line code:
+
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+  There's also [links](https://www.freecodecamp.com), and
+   > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+        - With different indentation levels.
+          - That look like this.
+
+And hey, here's a code block:
+\`\`\`
+Code block
+\`\`\`
+
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  `);
+  const inputChange = event => {
+    setMarkdownPreview(event.target.value);
+  };
+  return (
+    <div
+      style={{
+        marginTop: "12vh",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly"
+      }}
+    >
+      <div>
+        <textarea
+          id="editor"
+          onChange={inputChange}
+          style={{
+            width: 600,
+            height: 600
+          }}
+          value={markdownPreview}
+        ></textarea>
+      </div>
+
+      <div
+        id="preview"
+        style={{
+          width: 600,
+          height: 600,
+          border: "1px solid black",
+          fontSize: 10
+        }}
+      >
+        <ReactMarkdown source={markdownPreview} escapeHtml={false} />
+      </div>
+    </div>
+  );
+};
+
+export default MarkdownPreview;
